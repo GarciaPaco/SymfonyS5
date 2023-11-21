ffaf191959173172bd8c7d8d06bdd565df6760e1
Enstryl <machinchivu@gmail.com>
2023-11-21 10:12:14 +0100

    feat: Add token check to routes + fix categories group

--- my_project_directory/src/Entity/Category.php
+++ my_project_directory/src/Entity/Category.php
@@ -7,6 +7,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
@@ -21,6 +22,7 @@ class Category
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['category:read'])]
     #[Assert\NotBlank(message: 'Le nom est obligatoire')]
     private ?string $name = null;
 
