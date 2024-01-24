6b09626e3253d9d0d3355c11518dd8126aab4ce7
Enstryl <machinchivu@gmail.com>
2024-01-24 08:38:49 +0100

    Ajout Vich pour gérer l'upload et création du controller pour la route /me

---
+++ src/Controller/MeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class MeController extends AbstractController
+{
+    #[Route("/api/me", name:"get_current_user", methods:["GET"])]
+    public function getCurrentUser(UserInterface $user): JsonResponse
+    {
+        $userData = [
+            'email' => $user->getEmail(),
+            'firstname' => $user->getFirstname()
+            // etc....
+        ];
+
+        return new JsonResponse($userData);
+    }
+}
